de0eb132eea0dbe90d13c8b22a593101cb399670
肖越東 <xiaoyuedong1995@gmail.com>
2019-03-18 08:39:11 +0900

    resolve #6

--- src/main/java/com/xyd/webtask/entity/Goods.java
+++ src/main/java/com/xyd/webtask/entity/Goods.java
@@ -4,8 +4,8 @@ import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @GenericGenerator(name = "jpa-uuid", strategy = "uuid")
@@ -13,8 +13,7 @@ import javax.validation.constraints.Size;
 public class Goods {
 	
 	@Id
-	@NotNull
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 	private long id;
 	private String img;
 	@Size(max=100)
